ab0fd1dbfc69acad7d1699c7c4a511d360a78a62
Josh Marinacci <joshua@marinacci>
2022-08-11 13:49:11 -0400

    add some geometry and other useful classes

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,70 @@ export function make_logger(prefix?:string): Logger {
     return new ConsoleLogger(prefix?prefix:"")
 }
 
-
 export function sleep(sec: number): Promise<void> {
     return new Promise((res, rej) => {
         setTimeout(() => res(), Math.floor(sec * 1000))
     })
+}
+
+export function toRadians(param: number) {
+    return Math.PI * 2 / 360 * param
+}
+
+export function pick<T>(arr: T[]):T {
+    let n = Math.floor(Math.random() * arr.length)
+    return arr[n]
+}
+
+export class Point {
+    readonly x: number
+    readonly y: number
+
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+    add(pt: Point) {
+        return new Point(this.x+pt.x, this.y+pt.y)
+    }
+    subtract(pt: Point) {
+        return new Point(this.x-pt.x, this.y-pt.y)
+    }
+    magnitude() {
+        return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+    scale(scale: number) {
+        return new Point(this.x*scale,this.y*scale)
+    }
+    clone() {
+        return new Point(this.x,this.y)
+    }
+    toJSON() {
+        return {
+            x:this.x,
+            y:this.y,
+        }
+    }
+
+    static fromJSON(point: { x: number; y: number }) {
+        return new Point(point.x,point.y)
+    }
+}
+
+export class Size {
+    readonly w: number;
+    readonly h: number;
+    constructor(w:number, h:number) {
+        this.w = w
+        this.h = h
+    }
+}
+
+export class Bounds {
+    readonly position: Point;
+    readonly size: Size;
+    constructor(position:Point, size:Size) {
+        this.position = position
+        this.size = size
+    }
 }
