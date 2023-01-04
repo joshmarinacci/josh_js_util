eb7d3373a00ad844c9b65a4c33be0418107b9485
Josh Marinacci <joshua@marinacci>
2023-01-04 11:15:18 -0800

    added jarray functions with tests

--- src/jarray.ts
+++ src/jarray.ts
@@ -1,16 +1,36 @@
-/*
-- [ ] First
-- [ ] Last
-- [ ] Pluck
-- [ ] Tap
-- [ ] JArray.range to make a 0 to n array that you can map over.
-- [ ] Put in general js utils?
-*/
 
-
-
-export class JArray extends Array {
-    empty() {
+export class JArray<D> extends Array<D> {
+    static of<D>(...args:D[]): JArray<D> {
+        return new JArray(...args);
+    }
+    static range(first:number):JArray<number> {
+        let arr = new JArray<number>()
+        for(let i=0; i<first; i++) arr.push(i)
+        return arr
+    }
+    constructor(...args) {
+        super(...args)
+    }
+    map<Z>(fn:(val,index,arr)=>Z): JArray<Z> {
+        return super.map(fn) as JArray<Z>
+    }
+    tap(fn:(val,idnex,arr)=>void):JArray<D> {
+        this.forEach(fn)
+        return this
+    }
+    filter(fn:(val,index,arr)=>boolean): JArray<D> {
+        return super.filter(fn) as JArray<D>
+    }
+    pluck(name:keyof D):JArray<any> {
+        return this.map((x => x[name]))
+    }
+    first():D {
+        return this[0]
+    }
+    last():D {
+        return this[this.length-1]
+    }
+    empty():boolean {
         return this.length === 0
     }
 }
