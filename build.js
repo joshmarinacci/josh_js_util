5bcd1f6c829a3c4d884c58f8fb1d5dc042fa44eb
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-09-09 13:39:07 -0700

    add new build script

---
+++ build.js
@@ -0,0 +1,36 @@
+import { build } from "esbuild";
+import pkg from "npm-dts";
+const { Generator } = pkg;
+
+async function go() {
+  const outdir = "dist";
+  const sharedConfig = {
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: true,
+  };
+
+  await new Generator({
+    entry: "src/index.ts",
+    output: `${outdir}/index.d.ts`,
+  }).generate();
+
+  await build({
+    ...sharedConfig,
+    platform: "node",
+    outfile: `${outdir}/index.cjs`,
+    external: ["rtds-core", "react", "react-dom"],
+  });
+
+  await build({
+    ...sharedConfig,
+    platform: "neutral",
+    format: "esm",
+    outfile: `${outdir}/index.esm.js`,
+    external: ["rtds-core", "react", "react-dom"],
+  });
+}
+
+go()
+  .then(() => console.log("done"))
+  .catch((e) => console.log(e));
